e98680a6721ac25c4ecda1ac417ca70b1ac4f3c8
sensey2772-lgtm <sensey2772@gmail.com>
2026-03-25 12:19:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import flet as ft
 
+def main(page: ft.Page):
+    page.title = "Jarvis Test"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.add(
+        ft.Row(
+            [ft.Text("Приложение Jarvis работает!", size=25, color="blue")],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(target=main)
